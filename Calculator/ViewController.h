ddd86db3e8c6a01d1f0701dd519a9667801c04aa
Vini Gupta <vinig@thoughtworks.com>
2014-04-17 15:02:43 +0530

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Vini Gupta on 4/17/14.
+//  Copyright (c) 2014 Vini Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Vini Gupta on 4/17/14.
+//  Copyright (c) 2014 Vini Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
